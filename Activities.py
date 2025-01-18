756bcfb5f4a4ec811d5cb2a22e98a62818718b43
Hadil-8 <belaanes.hadil@gmail.com>
2025-01-18 21:16:22 +0100

    Update

---
+++ Activities.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+df = pd.read_csv('Activities.csv')
+
+
+print("Data from csv:")
+print(df)
---
+++ GuestHouses.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+df = pd.read_csv('GuestHouses.csv')
+
+
+print("Data from csv:")
+print(df)
